367b1fcbd63a95265c473a4d2391a47bd4e968a8
davidvvh <165288004+davidvvh@users.noreply.github.com>
2026-05-11 12:33:47 -0400

    ok

--- python/cores/Suscripción_plataforma_streaming(core)_david_2026_05_07/suscripcion_streaming.py
+++ python/cores/Suscripción_plataforma_streaming(core)_david_2026_05_07/suscripcion_streaming.py
@@ -1,30 +1,61 @@
-class SuscripcionStreaming:
-    costos_suscripcion = {"Gratis": 0, "Estándar": 5.99, "Premium": 10.99}
+class suscripcionStreaming:
+    costos_suscripcion = {"Gratis": 0, "Estandar": 5.99, "Premium": 10.99}
 
-    def __init__(self, usuario, costo_mensual, saldo_pendiente, tipo_suscripcion = "gratis"):
-        self.usuario = usuario
-        self.saldo_pendiente = saldo_pendiente
-        self.tipo_suscripcion = tipo_suscripcion
-
+#primera funcion: usuario y tipo_suscripcion
     def __init__(self, usuario, tipo_suscripcion="Gratis"):
-        pass
+        self.usuario = usuario
+        self.tipo_suscripcion = tipo_suscripcion
+        self.costo_mensual = self.costos_suscripcion[tipo_suscripcion]
+        self.saldo_pendiente = self.costo_mensual
+        print()
 
+#segunda funcion: monto
     def realizar_pago(self, monto):
-        """Reduce el saldo pendiente según el monto pagado."""
-        pass
+        self.saldo_pendiente = self.saldo_pendiente - monto
+        print("Usuario:", self.usuario)
+        print("Monto pagado:", monto)
+        print("Saldo actual:", self.saldo_pendiente)
 
+#tercera funcion: cambio de suscripcion
     def cambiar_suscripcion(self, nuevo_tipo):
-        """Cambia el tipo de suscripción y actualiza el costo mensual."""
-        SuscripcionStreaming.nuevo_tipo()
+        self.tipo_suscripcion = nuevo_tipo
+        self.costo_mensual = self.costos_suscripcion[nuevo_tipo]
+        self.saldo_pendiente = self.saldo_pendiente + self.costo_mensual
+        print("Cambio su tipo de plan a:", nuevo_tipo)
+        print()
 
+#cuarta funcion: ver contenido exclusivo
     def ver_contenido_exclusivo(self):
-        """Permite ver contenido exclusivo según el tipo de suscripción."""
-        pass
+        if self.tipo_suscripcion == "Gratis":
+            print("Acceso denegado")
+        else:
+            print("Acceso permitido para", self.usuario)
+            print()
 
+#ultima funcion: ver la info de la suscripcion
     def mostrar_info_suscripcion(self):
-        """Muestra la información de la suscripción del usuario."""
-        pass
+        print("Nombre:", self.usuario)
+        print("Suscripcion:", self.tipo_suscripcion)
+        print("Costo:", self.costo_mensual)
+        print("Saldo:", self.saldo_pendiente)
 
-usuario1 = SuscripcionStreaming()
-usuario2 = SuscripcionStreaming()
-usuario3 = SuscripcionStreaming()
+print("------------------Usuario Juan------------------")
+user1 = suscripcionStreaming("Juan", "Gratis")
+user1.ver_contenido_exclusivo()
+user1.cambiar_suscripcion("Estandar")
+user1.realizar_pago(5.99)
+user1.mostrar_info_suscripcion()
+
+print("------------------Usuario Carlos------------------")
+user2 = suscripcionStreaming("Carlos", "Estandar")
+user2.ver_contenido_exclusivo()
+user2.cambiar_suscripcion("Premium")
+user2.realizar_pago(7.00)
+user2.realizar_pago(10.00)
+user2.mostrar_info_suscripcion()
+
+print("------------------Usuario Patricio------------------")
+user3 = suscripcionStreaming("Patricio", "Premium")
+user3.realizar_pago(0)
+user3.ver_contenido_exclusivo()
+user3.mostrar_info_suscripcion()
